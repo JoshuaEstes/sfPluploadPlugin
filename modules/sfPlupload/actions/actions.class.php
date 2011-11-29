ff74c5e584c6bbeab23431a1adfc57a8d8b362c0
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2011-11-29 08:59:18 -0600

    bug fixes

--- modules/sfPlupload/actions/actions.class.php
+++ modules/sfPlupload/actions/actions.class.php
@@ -18,18 +18,18 @@ class sfPluploadActions extends sfActions
    */
   public function executeUpload(sfWebRequest $request)
   {
-    $this->getResponse()->setHttpHeader('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT');
-    $this->getResponse()->setHttpHeader('Last-Modified', gmdate("D, d M Y H:i:s") . ' GMT');
-    $this->getResponse()->setHttpHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
-    $this->getResponse()->setHttpHeader('Cache-Control', 'post-check=0, pre-check=0', false);
-    $this->getResponse()->setHttpHeader('Pragma', 'no-cache');
+//    $this->getResponse()->setHttpHeader('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT');
+//    $this->getResponse()->setHttpHeader('Last-Modified', gmdate("D, d M Y H:i:s") . ' GMT');
+//    $this->getResponse()->setHttpHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+//    $this->getResponse()->setHttpHeader('Cache-Control', 'post-check=0, pre-check=0', false);
+//    $this->getResponse()->setHttpHeader('Pragma', 'no-cache');
 
-    set_time_limit(5 * 60);
+    set_time_limit(15 * 60);
 
     $targetDir = sfConfig::get('sf_upload_dir');
 
-    $chunk = $request->getParameter('chunks', 0);
-    $chunks = $request->getParameter('chunk', 0);
+    $chunk = $request->getParameter('chunk', 0);
+    $chunks = $request->getParameter('chunks', 0);
     $fileName = $request->getParameter('name','');
 
     $fileName = preg_replace('/[^\w\._]+/', '', $fileName);
@@ -49,39 +49,63 @@ class sfPluploadActions extends sfActions
     }
 
     // Look for the content type header
+    $pathInfo = $request->getPathInfoArray();
     $contentType = '';
-    if(isset($_SERVER["HTTP_CONTENT_TYPE"]))
-      $contentType = $_SERVER["HTTP_CONTENT_TYPE"];
+    if(isset($pathInfo["CONTENT_TYPE"]))
+    {
+      $contentType = $pathInfo["CONTENT_TYPE"];
+    }
+    elseif(isset($pathInfo["HTTP_CONTENT_TYPE"]))
+    {
+      $contentType = $pathInfo["HTTP_CONTENT_TYPE"];
+    }
 
-    if(isset($_SERVER["CONTENT_TYPE"]))
-      $contentType = $_SERVER["CONTENT_TYPE"];
+    $files = $request->getFiles();
+    $files = $files['file'];
 
     // Handle non multipart uploads older WebKit versions didn't support multipart in HTML5
     if(strpos($contentType, "multipart") !== false)
     {
-      if(isset($_FILES['file']['tmp_name']) && is_uploaded_file($_FILES['file']['tmp_name']))
+      if (isset($files['error']) && $files['error'])
+      {
+        die(sprintf('{"jsonrpc": "2.0", "error" : { "message": "%s" }}',$files['error']));
+      }
+      if(isset($files['tmp_name']) && is_uploaded_file($files['tmp_name']))
       {
         // Open temp file
         $out = fopen($targetDir . DIRECTORY_SEPARATOR . $fileName, $chunk == 0 ? "wb" : "ab");
         if($out)
         {
           // Read binary input stream and append it to temp file
-          $in = fopen($_FILES['file']['tmp_name'], "rb");
+          $in = fopen($files['tmp_name'], "rb");
 
           if($in)
           {
             while($buff = fread($in, 4096))
+            {
               fwrite($out, $buff);
-          } else
+            }
+          }
+          else
+          {
             die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+          }
           fclose($in);
           fclose($out);
-          @unlink($_FILES['file']['tmp_name']);
-        } else
+          unlink($files['tmp_name']);
+        }
+        else
+        {
           die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
-      } else
+        }
+      }
+      else
+      {
+        die(var_dump($files));
         die('{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}');
-    } else
+      }
+    }
+    else
     {
       // Open temp file
       $out = fopen($targetDir . DIRECTORY_SEPARATOR . $fileName, $chunk == 0 ? "wb" : "ab");
@@ -101,6 +125,11 @@ class sfPluploadActions extends sfActions
         fclose($out);
       } else
         die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+    }
+
+    if ($chunks == ($chunk + 1))
+    {
+      die('{"jsonrpc" : "2.0", "result" : "complete", "id" : "id"}');
     }
 
     // Return JSON-RPC response
